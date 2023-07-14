153157758fd812d7c52f58faca467c8cf536b16d
walaa2022 <100930821+walaa2022@users.noreply.github.com>
2023-07-14 11:07:38 +0300

    Update app_3rd.py

--- app_3rd.py
+++ app_3rd.py
@@ -52,7 +52,7 @@ def main():
 
     
     if st.button("predict"):
-        result = prediction(online_order, book_table, votes, location,approx_cost,listed_in,listed_in_city,cuisines_counts,rest_type_counts)
+        result = prediction(Gender, Married, Dependents, Education, Self_Employed, Credit_History, Property_Area, LoanAmount_log, Total_Income, EMI, Total_Income_log, Balance_Income)
         label = ["Approved" , "Not-approved"]
         st.text(f"The Loan will be {label[result]}")
         
